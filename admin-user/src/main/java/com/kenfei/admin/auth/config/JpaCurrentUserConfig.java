6178c41e6b3a95672ef46250b4f9e3738436bedd
kenfei <kenfei@aliyun.com>
2021-11-11 15:54:24 +0800

    user 模块添加 jpa 的 entity 扫包配置

--- admin-user/src/main/java/com/kenfei/admin/auth/config/JpaCurrentUserConfig.java
+++ admin-user/src/main/java/com/kenfei/admin/auth/config/JpaCurrentUserConfig.java
@@ -1,6 +1,7 @@
 package com.kenfei.admin.auth.config;
 
 import com.kenfei.admin.auth.model.CurrentUser;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -15,6 +16,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @Configuration
 @EnableJpaAuditing
+@EntityScan(basePackages = {"com.kenfei.admin.**.entity"})
 @EnableJpaRepositories(basePackages = {"com.kenfei.admin.**.repository"})
 public class JpaCurrentUserConfig {
   /** 注入当前登陆用户名 */
